a78324741925a779895c559662af02f75bbfa8b4
João D. Ferreira <jotomicron@gmail.com>
2020-09-10 00:33:54 +0000

    Change the pre-commit verification step
    Previously, the code was fixed (php-cs-fixer) automatically, which could
    cause merge conflicts with the stashed un-commited changes. Now, the
    pre-commit hook simply checks that there are style coding issues and
    refuses to commit if there are, saying so. This forces the developer to
    stash the changes themselves.
    
    A better approach would be to automatically fix files that do not have
    any un-staged changes, but that will maybe be done in the future.

--- hooks/post-commit.php
+++ hooks/post-commit.php
@@ -2,4 +2,9 @@
 <?php
 
 // Because our pre-commit created a stash, we need to pop it now
-exec('git stash pop');
+// However, only do this if there is really something to pop
+exec('git stash list | grep precommit-verification | cut -d: -f1 | head -n 1', $stashes);
+
+if (count($stashes) === 1) {
+    exec("git stash pop {$stashes[0]}");
+}
--- hooks/pre-commit.php
+++ hooks/pre-commit.php
@@ -1,79 +1,53 @@
 #!/usr/bin/env php
 <?php
 
-// Let's save the non-staged files
-exec('git stash save --keep-index --include-untracked');
+// Let's save the non-staged code into stash `precommit-verification`
+exec(
+    'git stash push -m precommit-verification --keep-index --include-untracked'
+);
 
-// Run phpunit before committing and make sure all tests pass
-exec('script --return --quiet -c "phpunit --exclude-group end-to-end" /dev/null', $output, $code);
+// If the code is not properly formatted, stop the commit process. In that case,
+// let the user know and pop the stash.
+exec('php-cs-fixer fix --dry-run >/dev/null 2>&1', $_, $exitCode);
 
-if ($code !== 0) {
-    echo "\033[31m"; // Red text
-    echo 'PHPUnit failed';
-    echo "\033[0m"; // Reset
-    echo ' - see output below' . PHP_EOL;
-
+if ($exitCode !== 0) {
+    echo "\033[32m"; // Green text
+    echo 'Code style fixes are required';
+    echo "\033[m";
     echo PHP_EOL;
-    echo implode(PHP_EOL, $output) . PHP_EOL;
-    echo PHP_EOL;
 
-    exec('git stash pop');
+    echo 'Use php-cs-fixer fix to fix them' . PHP_EOL;
+    echo '(No automatic format is done to prevent merge conflicts with the files that contain unstaged code)' . PHP_EOL;
+
+    exec('git stash pop $(git stash list | grep precommit-verification | cut -d: -f1 | head -n 1)');
 
     exit(1);
 }
 
-// Get a list of files in the staging area
-exec('git status --porcelain | egrep "^[AM]" | cut -c4-', $staged);
+// Run phpunit before committing and make sure all tests pass
+$specs = [
+    0 => ['file', '/dev/null', 'r'],
+    1 => ['pipe', 'w'],
+    2 => ['pipe', 'w'],
+];
 
-$fixed = [];
+$pupunitProcess = proc_open('phpunit --exclude-group end-to-end', $specs, $pipes);
 
-foreach ($staged as $filename) {
-    // Unescape escaped charaters
-    if (preg_match('/^".*"$/', $filename)) {
-        $unescaped = stripcslashes(substr($filename, 1, -1));
-    } else {
-        $unescaped = $filename;
-    }
+stream_copy_to_stream($pipes[1], STDOUT);
+fclose($pipes[1]);
 
-    // is_file - to avoid problems with "renamed" and "deleted" files.
-    if (preg_match('/\.php$/', $unescaped) && is_file($unescaped)) {
-        $output = [];
+stream_copy_to_stream($pipes[2], STDERR);
+fclose($pipes[2]);
 
-        // Record the modification time of the file to determine in any fixes
-        // were applied
-        $modified_at = filemtime($unescaped);
-
-        exec(sprintf('php-cs-fixer fix "%s" 2>/dev/null', $unescaped), $output);
-
-        // Clear the cache that stores the modification date of a file so that
-        // we can detect changes.
-        clearstatcache($unescaped);
-
-        if (filemtime($filename) > $modified_at) {
-            // The file has been fixed in some way or another. As such, re-add
-            // it back to the staging area. Notice that this will likely cause
-            // merging issues when we pop back the stash we saved before. If
-            // those exist, we should simply force the stashed version to
-            // overwrite what php-cs-fixer fixed. TODO: Let's try to do that in
-            exec(sprintf('git add "%s"', $unescaped));
-
-            $fixed[] = $filename;
-        }
-    }
-}
-
-if (count($fixed) > 0) {
-    echo "\033[32m"; // Green text
-    echo 'Code style fixes were applied to the following files:';
+if (proc_close($pupunitProcess) !== 0) {
+    echo "\033[31m"; // Red text
+    echo 'PHPUnit failed';
     echo "\033[m";
-    echo PHP_EOL . PHP_EOL;
+    echo PHP_EOL;
 
-    $counter = 1;
+    exec('git stash pop $(git stash list | grep precommit-verification | cut -d: -f1 | head -n 1)');
 
-    foreach ($fixed as $filename) {
-        echo '  ' . $counter . ') ' . $filename . PHP_EOL;
-        $counter++;
-    }
+    exit(1);
 }
 
 exit(0);
