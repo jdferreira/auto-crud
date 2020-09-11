0f09e7aabe0b016625219fe6cc794bd697930dad
João D. Ferreira <jotomicron@gmail.com>
2020-09-11 14:42:15 +0000

    Begin speeding up tests
    The Generator tests depend upon actual migrations that must run and be
    refreshed every test, which is slow and unnecessary. This commit begins
    the process of making the tests not dependent on actual migrations, but
    to depend instead on mocked TableInformation (and possibly
    DatabaseInformation) instances, which do not require migrations to run
    and be cleaned afterwards, with the added benefit that the tests are
    less coupled.
    
    This commit changed the ControllerGenerator tests as a start. The
    running times for this test suite went from about 1.27 seconds to .48
    seconds (a speed-up of 2.6x)!

--- tests/Unit/ControllerGeneratorTest.php
+++ tests/Unit/ControllerGeneratorTest.php
@@ -9,53 +9,49 @@ use Ferreira\AutoCrud\Generators\ControllerGenerator;
 class ControllerGeneratorTest extends TestCase
 {
     /**
-     * The directory holding the migrations for these tests.
-     *
-     * @var string
-     */
-    protected $migrations = __DIR__ . '/../migrations';
-
-    /**
      * Create a generator that can be used to generate or save the expected file.
      *
-     * @param string $table
-     * @param string $dir
+     * @param TableInformation $table
      *
      * @return ControllerGenerator
      */
-    private function generator(string $table): ControllerGenerator
+    private function generator($table): ControllerGenerator
     {
-        return app(ControllerGenerator::class, [
-            'table' => app(TableInformation::class, ['name' => $table]),
-        ]);
+        return app(ControllerGenerator::class, ['table' => $table]);
     }
 
     /** @test */
     public function it_can_generate_a_controller()
     {
-        $this->generator('users')->save();
+        $this->generator(
+            $this->mockTable('students')
+        )->save();
 
-        $this->assertFileExists(app_path('Http/Controllers/UserController.php'));
+        $this->assertFileExists(app_path('Http/Controllers/StudentController.php'));
     }
 
     /** @test */
     public function it_detects_model_namespace()
     {
-        $code = $this->generator('users')->setModelDirectory('Models')->save();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->setModelDirectory('Models')->generate();
 
         $this->assertStringContainsString(
-            'use App\Models\User;',
-            $this->files->get(app_path('Http/Controllers/UserController.php'))
+            'use App\Models\Student;',
+            $code
         );
     }
 
     /** @test */
     public function it_uses_the_tablename_to_name_the_controller()
     {
-        $code = $this->generator('products')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertStringContainsString(
-            'class ProductController extends Controller',
+            'class StudentController extends Controller',
             $code
         );
     }
@@ -63,10 +59,12 @@ class ControllerGeneratorTest extends TestCase
     /** @test */
     public function it_uses_the_correct_form_request()
     {
-        $code = $this->generator('users')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertStringContainsString(
-            'use App\Http\Requests\UserRequest;',
+            'use App\Http\Requests\StudentRequest;',
             $code
         );
     }
@@ -74,7 +72,9 @@ class ControllerGeneratorTest extends TestCase
     /** @test */
     public function it_generates_the_six_crud_standard_methods()
     {
-        $code = $this->generator('users')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $views = [
             'index',
@@ -94,171 +94,125 @@ class ControllerGeneratorTest extends TestCase
     /** @test */
     public function it_generates_a_correct_index()
     {
-        $users = $this->generator('users')->generate();
-        $avatars = $this->generator('avatars')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertCodeContains("
             public function index()
             {
-                return view('users.index', ['users' => User::paginate()]);
+                return view('students.index', ['students' => Student::paginate()]);
             }
-        ", $users);
-
-        $this->assertCodeContains("
-            public function index()
-            {
-                return view('avatars.index', ['avatars' => Avatar::paginate()]);
-            }
-        ", $avatars);
+        ", $code);
     }
 
     /** @test */
     public function it_generates_a_correct_show()
     {
-        $users = $this->generator('users')->generate();
-        $avatars = $this->generator('avatars')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertCodeContains("
-            public function show(User \$user)
+            public function show(Student \$student)
             {
-                return view('users.show', compact('user'));
+                return view('students.show', compact('student'));
             }
-        ", $users);
-
-        $this->assertCodeContains("
-            public function show(Avatar \$avatar)
-            {
-                return view('avatars.show', compact('avatar'));
-            }
-        ", $avatars);
+        ", $code);
     }
 
     /** @test */
     public function it_generates_a_correct_create()
     {
-        $users = $this->generator('users')->generate();
-        $avatars = $this->generator('avatars')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertCodeContains("
             public function create()
             {
-                return view('users.create');
+                return view('students.create');
             }
-        ", $users);
-
-        $this->assertCodeContains("
-            public function create()
-            {
-                return view('avatars.create');
-            }
-        ", $avatars);
+        ", $code);
     }
 
     /** @test */
     public function it_generates_a_correct_store()
     {
-        $users = $this->generator('users')->generate();
-        $avatars = $this->generator('avatars')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertCodeContains('
-            public function store(UserRequest $request)
+            public function store(StudentRequest $request)
             {
-                $user = User::create($request->validated());
+                $student = Student::create($request->validated());
 
-                return redirect($user->path());
+                return redirect($student->path());
             }
-        ', $users);
-
-        $this->assertCodeContains('
-            public function store(AvatarRequest $request)
-            {
-                $avatar = Avatar::create($request->validated());
-
-                return redirect($avatar->path());
-            }
-        ', $avatars);
+        ', $code);
     }
 
     /** @test */
     public function it_generates_a_correct_edit()
     {
-        $users = $this->generator('users')->generate();
-        $avatars = $this->generator('avatars')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertCodeContains("
-            public function edit(User \$user)
+            public function edit(Student \$student)
             {
-                return view('users.edit', compact('user'));
+                return view('students.edit', compact('student'));
             }
-        ", $users);
-
-        $this->assertCodeContains("
-            public function edit(Avatar \$avatar)
-            {
-                return view('avatars.edit', compact('avatar'));
-            }
-        ", $avatars);
+        ", $code);
     }
 
     /** @test */
     public function it_generates_a_correct_update()
     {
-        $users = $this->generator('users')->generate();
-        $avatars = $this->generator('avatars')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertCodeContains('
-            public function update(User $user, UserRequest $request)
+            public function update(Student $student, StudentRequest $request)
             {
-                $user->update($request->validated());
+                $student->update($request->validated());
 
-                return redirect($user->path());
+                return redirect($student->path());
             }
-        ', $users);
-
-        $this->assertCodeContains('
-            public function update(Avatar $avatar, AvatarRequest $request)
-            {
-                $avatar->update($request->validated());
-
-                return redirect($avatar->path());
-            }
-        ', $avatars);
+        ', $code);
     }
 
     /** @test */
     public function it_generates_a_correct_destroy()
     {
-        $users = $this->generator('users')->generate();
-        $avatars = $this->generator('avatars')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertCodeContains("
-            public function destroy(User \$user)
+            public function destroy(Student \$student)
             {
-                \$user->delete();
+                \$student->delete();
 
-                return redirect(route('users.index'));
+                return redirect(route('students.index'));
             }
-        ", $users);
-
-        $this->assertCodeContains("
-            public function destroy(Avatar \$avatar)
-            {
-                \$avatar->delete();
-
-                return redirect(route('avatars.index'));
-            }
-        ", $avatars);
+        ", $code);
     }
 
     /** @test */
     public function it_uses_correct_view_names()
     {
-        $code = $this->generator('payment_methods')->generate();
+        $code = $this->generator(
+            $this->mockTable('detention_methods')
+        )->generate();
 
         $this->assertCodeContains('
             public function index()
             {
-                return view(\'payment_methods.index\', [\'paymentMethods\' => PaymentMethod::paginate()]);
+                return view(\'detention_methods.index\', [\'detentionMethods\' => DetentionMethod::paginate()]);
             }
         ', $code);
     }
