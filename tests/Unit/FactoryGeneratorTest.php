19f4c88cbb9469f9524bc2d2820a5cd045b0efe0
João D. Ferreira <jotomicron@gmail.com>
2020-09-11 23:49:39 +0000

    Improve tests for `FactoryGenerator`

--- tests/Unit/FactoryGeneratorTest.php
+++ tests/Unit/FactoryGeneratorTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
+use Mockery;
 use Tests\TestCase;
+use Ferreira\AutoCrud\Type;
 use Ferreira\AutoCrud\Generators\ColumnFaker;
 use Ferreira\AutoCrud\Database\TableInformation;
 use Ferreira\AutoCrud\Generators\FactoryGenerator;
@@ -10,181 +12,101 @@ use Ferreira\AutoCrud\Generators\FactoryGenerator;
 class FactoryGeneratorTest extends TestCase
 {
     /**
-     * The directory holding the migrations for these tests.
-     *
-     * @var string
-     */
-    protected $migrations = __DIR__ . '/../migrations';
-
-    /**
      * Create a generator that can be used to generate or save the expected file.
      *
-     * @param string $table
-     * @param string $dir
+     * @param TableInformation $table
      *
      * @return FactoryGenerator
      */
-    private function generator(string $table): FactoryGenerator
+    private function generator($table): FactoryGenerator
     {
-        return app(FactoryGenerator::class, [
-            'table' => app(TableInformation::class, ['name' => $table]),
-        ]);
+        return app(FactoryGenerator::class, ['table' => $table]);
     }
 
     /** @test */
     public function it_can_generate_a_factory()
     {
-        $this->generator('users')->save();
+        $this->generator(
+            $this->mockTable('students')
+        )->save();
 
-        $this->assertFileExists(database_path('factories/UserFactory.php'));
-    }
-
-    /** @test */
-    public function it_detects_model_namespace()
-    {
-        $code = $this->generator('users')->setModelDirectory('Models')->generate();
-
-        $this->assertStringContainsString(
-            'use App\Models\User;',
-            $code
-        );
+        $this->assertFileExists(database_path('factories/StudentFactory.php'));
     }
 
     /** @test */
     public function it_detects_referenced_models_qualified_name()
     {
-        $code = $this->generator('products')->generate();
-        $this->assertStringContainsString('use App\User;', $code);
+        $generator = $this->generator(
+            $this->mockTable('students')
+        );
 
-        $code = $this->generator('products')->setModelDirectory('Models')->generate();
-        $this->assertStringContainsString('use App\Models\User;', $code);
+        $code = $generator->generate();
+        $this->assertStringContainsString('use App\Student;', $code);
+
+        $code = $generator->setModelDirectory('Models')->generate();
+        $this->assertStringContainsString('use App\Models\Student;', $code);
     }
 
     /** @test */
     public function it_defines_a_factory()
     {
-        $code = $this->generator('products')->generate();
+        $code = $this->generator(
+            $this->mockTable('students')
+        )->generate();
 
         $this->assertStringContainsString('use Faker\Generator as Faker;', $code);
-        $this->assertStringContainsString('$factory->define(Product::class', $code);
+        $this->assertStringContainsString('$factory->define(Student::class', $code);
     }
 
     /** @test */
     public function it_uses_column_definitions_to_decide_how_to_fake()
     {
-        $this->app->bind(ColumnFaker::class, function () {
+        $this->app->bind(ColumnFaker::class, function ($param) {
             return $this->mock(ColumnFaker::class, function ($mock) {
                 $mock->shouldReceive('fake')->once();
                 $mock->shouldReceive('referencedTable')->atMost()->times(1);
             });
         });
 
-        $this->generator('users')->generate();
-    }
+        // We need to have at least one column to ensure that the ColumnFaker
+        // class is instantiated at least once.
+        $table = $this->mockTable('students', [
+            'name' => [],
+        ]);
 
-    /** @test */
-    public function it_generates_correct_factory_for_user()
-    {
-        $code = $this->generator('users')->generate();
-
-        $this->assertCodeContains("
-            return [
-                'name' => \$faker->name,
-                'email' => \$faker->randomFloat() <= 0.9 ? \$faker->unique()->email : null,
-                'subscribed' => \$faker->boolean,
-                'birthday' => \$faker->date,
-                'wake_up' => \$faker->optional(0.9)->time,
-            ];
-        ", $code);
-    }
-
-    /** @test */
-    public function it_generates_correct_factory_for_avatar()
-    {
-        $code = $this->generator('avatars')->generate();
-
-        $this->assertCodeContains("
-            return [
-                'user_id' => function () {
-                    return factory(User::class)->create()->id;
-                },
-                'file' => \$faker->file,
-            ];
-        ", $code);
-    }
-
-    /** @test */
-    public function it_generates_correct_factory_for_product()
-    {
-        $code = $this->generator('products')->generate();
-
-        $this->assertCodeContains("
-            return [
-                'owner_id' => function () {
-                    return factory(User::class)->create()->id;
-                },
-                'type' => \$faker->optional(0.9)->randomElement(['food', 'stationery', 'other']),
-                'value' => \$faker->numerify('%##.##'),
-                'start_at' => \$faker->dateTimeBetween('-10 years', 'now')->format('Y-m-d H:i:s'),
-            ];
-        ", $code);
-    }
-
-    /** @test */
-    public function it_generates_correct_factory_for_role()
-    {
-        $code = $this->generator('roles')->generate();
-
-        $this->assertCodeContains("
-            return [
-                'name' => \$faker->name,
-                'description' => \$faker->sentence,
-            ];
-        ", $code);
-    }
-
-    /** @test */
-    public function it_generates_correct_factory_for_sale()
-    {
-        $code = $this->generator('sales')->generate();
-
-        $this->assertCodeContains("
-            return [
-                'product_id' => function () {
-                    return factory(Product::class)->create()->product_id;
-                },
-                'amount' => \$faker->numberBetween(0, 10000),
-                'date' => \$faker->date,
-            ];
-        ", $code);
+        $this->generator($table)->generate();
     }
 
     /** @test */
     public function it_creates_a_full_model_state_for_models_with_nullable_columns()
     {
-        $code = $this->generator('users')->generate();
-        $this->assertCodeContains("
-            \$factory->state(User::class, 'full_model', function (Faker \$faker) {
-                return [
-                    'email' => \$faker->unique()->email,
-                    'wake_up' => \$faker->time,
-                ];
-            });
-        ", $code);
+        $code = $this->generator(
+            $this->mockTable('students', [
+                'pet' => [
+                    'type' => Type::STRING,
+                    'required' => false,
+                ],
+            ])
+        )->generate();
 
-        $code = $this->generator('sales')->generate();
-        $this->assertCodeContains("
-            \$factory->state(Sale::class, 'full_model', function (Faker \$faker) {
-                return [
-                ];
-            });
-        ", $code);
+        $this->assertStringContainsString(
+            "\$factory->state(Student::class, 'full_model'",
+            $code
+        );
     }
 
     /** @test */
     public function it_ignores_deleted_at_columns()
     {
-        $code = $this->generator('products')->generate();
+        $code = $this->generator(
+            $this->mockTable('students', [
+                'deleted_at' => [
+                    'type' => Type::DATETIME,
+                    'required' => false,
+                ],
+            ])
+        )->generate();
+
         $this->assertStringNotContainsString("'deleted_at' => ", $code);
     }
 }
